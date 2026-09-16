cd1c8811eee2af156cde17139bc53afa8c06c1eb
lzsouza987 <luizfernando987@gmail.com>
2026-09-16 20:30:39 -0300

    Aula 08 - RpgApi - Operações CRUD com Entity Framework

--- obj/Debug/net10.0/RpgApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/RpgApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a63332d7f56e6111d08181a3bb3180377fae4c49")]
 [assembly: System.Reflection.AssemblyProductAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
